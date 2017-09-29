a0722f121c18059289e93eaf5fafdcf13993a265
nooldey <openzws@163.com>
2017-09-29 17:05:38 +0800

    f

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ Choose your operation:
     2. stop DB
 " flag
 
+#开启MongoDB
 start() {
     echo 'robot going to start DB...'
     sleep 0.3
@@ -14,6 +15,8 @@ start() {
     sleep 0.1
     echo 'MongoDB is running now!'
 }
+
+#关闭MongoDB
 stop() {
     echo 'robot will stop DB...'
     sleep 0.3
@@ -22,6 +25,7 @@ stop() {
     echo 'MongoDB is stop now!'
 }
 
+#处理用户输入
 catdb(){
     case $1 in
         1)
